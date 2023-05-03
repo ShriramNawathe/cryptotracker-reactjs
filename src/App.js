685c33c6eb3fc7538395b79b331ccaa14d3a7a32
Shriram26101999 <shriramnawathe@gmail.com>
2023-05-04 00:09:47 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+import DashboardPage from "./pages/dashboard";
+import CoinPage from "./pages/coin";
+import ComparePage from "./pages/compare";
+import WatchlistPage from "./pages/watchlist";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import Footer from "./components/Common/Footer/footer";
+
+function App() {
+  var cursor;
+  var cursorPointer;
+
+  useEffect(() => {
+    cursor = document.getElementById("cursor");
+    cursorPointer = document.getElementById("cursor-pointer");
+
+    document.body.addEventListener("mousemove", function (e) {
+      return (
+        (cursor.style.left = e.clientX + "px"),
+        (cursor.style.top = e.clientY + "px"),
+        (cursorPointer.style.left = e.clientX + "px"),
+        (cursorPointer.style.top = e.clientY + "px")
+      );
+    });
+
+    document.body.addEventListener("mousedown", function (e) {
+      return (
+        (cursor.style.height = "0.5rem"),
+        (cursor.style.width = "0.5rem"),
+        (cursorPointer.style.height = "3rem"),
+        (cursorPointer.style.width = "3rem")
+      );
+    });
+
+    document.body.addEventListener("mouseup", function (e) {
+      return (
+        (cursor.style.height = "0.3rem"),
+        (cursor.style.width = "0.3rem"),
+        (cursorPointer.style.height = "2rem"),
+        (cursorPointer.style.width = "2rem")
+      );
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="cursor" id="cursor" />
+      <div className="cursor-pointer" id="cursor-pointer" />
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/compare" element={<ComparePage />} />
+          <Route path="/watchlist" element={<WatchlistPage />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Common/Footer/footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./styles.css";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from "@mui/icons-material/Email";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+function Footer() {
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  return (
+    <div className="footer">
+      <h2 className="logo" onClick={() => topFunction()}>
+        CryptoTracker<span>.</span>
+      </h2>
+      <div className="social-links">
+        <a href="mailto:shrinawathe@gmail.com">
+          <EmailIcon className="social-link" />
+        </a>
+        <a href="www.linkedin.com/in/shriram-nawathe">
+          <LinkedInIcon className="social-link" />
+        </a>
+        <a href="https://www.instagram.com/imshreeeeee/?igshid=ZDdkNTZiNTM">
+          <InstagramIcon className="social-link" />
+        </a>
+        <a href="https://www.twitter.com">
+          <TwitterIcon className="social-link" />
+        </a> 
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
